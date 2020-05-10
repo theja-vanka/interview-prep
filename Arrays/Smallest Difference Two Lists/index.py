36c41909d6b37b99aa4bde3444864b4c70e01607
Krishnatheja Vanka <theja.vanka@gmail.com>
2020-05-10 07:02:27 +0530

    Sort added

--- Arrays/Smallest Difference Two Lists/index.py
+++ Arrays/Smallest Difference Two Lists/index.py
@@ -1,20 +1,22 @@
 def smallestDifference(arrayOne, arrayTwo):
+    arrayOne.sort()
+    arrayTwo.sort()
     indexone = 0
-	indextwo = 0
-	current = float('inf')
-	smallest = float('inf')
-	while indexone < len(arrayOne) and indextwo < len(arrayTwo):
-		firstNumber = arrayOne[indexone]
-		secondNumber = arrayTwo[indextwo]
-		if firstNumber < secondNumber:
-			current = secondNumber - firstNumber
-			indexone += 1
-		elif firstNumber > secondNumber:
-			current = firstNumber - secondNumber
-			indextwo += 1
-		else:
-			return [firstNumber,secondNumber]
-		if smallest > current:
-			smallest = current
-			smallestPair = [firstNumber, secondNumber]
+    indextwo = 0
+    current = float('inf')
+    smallest = float('inf')
+    while indexone < len(arrayOne) and indextwo < len(arrayTwo):
+        firstNumber = arrayOne[indexone]
+        secondNumber = arrayTwo[indextwo]
+    if firstNumber < secondNumber:
+        current = secondNumber - firstNumber
+        indexone += 1
+    elif firstNumber > secondNumber:
+        current = firstNumber - secondNumber
+        indextwo += 1
+    else:
+        return [firstNumber, secondNumber]
+    if smallest > current:
+        smallest = current
+        smallestPair = [firstNumber, secondNumber]
     return smallestPair
