3ed1d5f80bc7ecfaeefef0b2d1dd3691c9a04f83
Krishnatheja Vanka <theja.vanka@gmail.com>
2020-05-11 20:27:27 +0530

    Bubble Sort

---
+++ Sorting/Readme.md
@@ -0,0 +1,7 @@
+Write a function that takes in an array of integers and returns a sorted version of that array. Use the Bubble Sort algorithm to sort the array.
+
+Example:<br>
+Input:<br>
+array = [8, 5, 2, 9, 5, 6, 3]<br>
+Output:<br>
+[2, 3, 5, 5, 6, 8, 9]
---
+++ Sorting/index.py
@@ -0,0 +1,23 @@
+def bubbleSort(array):
+    for index in range(len(array)):
+		for jdex in range(index, len(array), 1):
+			if array[index] > array[jdex]:
+				temp = array[index]
+				array[index] = array[jdex]
+				array[jdex] = temp
+	return array
+
+def bubbleSortAlternate(array):
+    isSorted = False
+    counter = 0
+    while not isSorted:
+        isSorted = True
+        for i in range(len(array) - 1 - counter):
+            if array[i] > array[i + 1]:
+                swap(i, i + 1, array)
+                isSorted = False
+        counter += 1
+    return array
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
