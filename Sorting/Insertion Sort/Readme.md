23ef40170e505350cf94251de5ef746d62d1a80f
Krishnatheja Vanka <theja.vanka@gmail.com>
2020-05-23 16:01:02 +0530

    Insertion Sort

---
+++ Sorting/Insertion Sort/Readme.md
@@ -0,0 +1,1 @@
+Write a function that takes in an array of integers and returns a sorted version of that array. Use the Insertion Sort algorithm to sort the array.
---
+++ Sorting/Insertion Sort/index.py
@@ -0,0 +1,21 @@
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            swap(j, j - 1, array)
+            j -= 1
+    return array
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
+
+
+def insertionSortAlternative(array):
+    for i in range(1, len(array)):
+        for j in range(i, 0, -1):
+            if array[j] < array[j - 1]:
+                array[j], array[j-1] = array[j-1], array[j]
+            else:
+                pass
+    return array
