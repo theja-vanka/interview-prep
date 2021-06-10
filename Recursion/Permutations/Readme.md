155159ec5b4da7c11aa16f1f77647c240590b732
Krishnatheja Vanka <theja.vanka@gmail.com>
2021-06-10 22:09:32 +0530

    Add Permutations

---
+++ Recursion/Permutations/Readme.md
@@ -0,0 +1,9 @@
+Write a function that takes in an array of unique integers and returns an array of all permutations of those integers in no particular order.
+
+If the input array is empty, the function should return an empty array.</p>
+
+Example:<br>
+Input:<br>
+array = [1, 2, 3]<br>
+Output:<br>
+[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
---
+++ Recursion/Permutations/index.py
@@ -0,0 +1,34 @@
+def getPermutations(array):
+    permutations = []
+    permutationsHelper(array, [], permutations)
+    return permutations
+
+
+def permutationsHelper(array, currentPermutation, permutations):
+    if not len(array) and len(currentPermutation):
+        permutations.append(currentPermutation)
+    else:
+        for i in range(len(array)):
+            newArray = array[:i] + array[i+1:]
+            newPermutation = currentPermutation + [array[i]]
+            permutationsHelper(newArray, newPermutation, permutations)
+
+
+def getPermutationsAlt(array):
+    permutations = []
+    permutationsHelperAlt(0, array, permutations)
+    return permutations
+
+
+def permutationsHelperAlt(i, array, permutations):
+    if i == len(array) - 1:
+        permutations.append(array[:])
+    else:
+        for j in range(i, len(array)):
+            swap(array, i, j)
+            permutationsHelper(i + 1, array, permutations)
+            swap(array, i, j)
+
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
